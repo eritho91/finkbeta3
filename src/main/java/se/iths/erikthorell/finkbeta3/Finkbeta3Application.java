522eb3cf47146b8e042279e46718d4b793531fc4
eritho91 <erik.thorell@iths.se>
2026-03-30 14:28:37 +0200

    init commit

---
+++ src/main/java/se/iths/erikthorell/finkbeta3/Finkbeta3Application.java
@@ -0,0 +1,13 @@
+package se.iths.erikthorell.finkbeta3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finkbeta3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Finkbeta3Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/erikthorell/finkbeta3/Finkbeta3ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.erikthorell.finkbeta3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finkbeta3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
